54e56f80728a2249941b003246d3b5290ebeee7a
melinda75 <38343507+melinda75@users.noreply.github.com>
2018-08-01 14:49:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Healthy Subscribe Form Responsive Widget Template :: w3layouts</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Healthy Subscribe Form template Responsive, Login form web template,Flat Pricing tables,Flat Drop downs  Sign up Web Templates, Flat Web Templates, Login sign up Responsive web template, SmartPhone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"
+	/>
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- Custom Theme files -->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- //Custom Theme files -->
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- web font -->
+	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<!-- //web font -->
+</head>
+
+<body>
+	<h1>
+		<span>H</span>ealthy
+		<span>S</span>ubscribe
+		<span>F</span>orm
+	</h1>
+	<!-- main-content -->
+	<div class="main-agileinfo">
+		<h6>Enter your email to get</h6>
+		<h2>Healthy Living Newsletter</h2>
+		<form action="#" method="post">
+			<div class="form-mail">
+				<span class="fa fa-envelope-o" aria-hidden="true"></span>
+				<input type="email" name="email" placeholder="Enter your Email..." required="">
+			</div>
+			<input type="submit" value="Subscribe">
+		</form>
+		<h3>
+			<span class="fa fa-apple" aria-hidden="true"></span>Everyday
+			<label>Health</label>
+		</h3>
+	</div>
+	<!-- //main-content -->
+	<!-- copyright -->
+	<div class="w3copyright-agile">
+		<p>© 2018 Healthy Subscribe Form. All rights reserved | Design by
+			<a href="http://w3layouts.com/" target="_blank">W3layouts</a>
+		</p>
+	</div>
+	<!-- //copyright -->
+</body>
+
+</html>
